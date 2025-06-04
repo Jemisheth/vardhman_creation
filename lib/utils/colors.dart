3e7b98743eec3f3dace2312b84f7705e37c39d6e
Jemisheth <jemis0703@gmail.com>
2025-06-04 11:11:57 +0530

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+// primary colors for app
+Color primaryColorShade0 = const Color(0xff2F648E);
+Color primaryColorShade1 = const Color(0xffCDE3F5);
+Color primaryColorShade2 = const Color(0xffD1E8FF);
+Color primaryColorShade3 = const Color(0xffE7F0F5);
+Color primaryColorShade4 = const Color(0xffBFD9EA);
+Color primaryColorShade5 = const Color(0xffEAF0F4);
+Color primaryColorShade6 = const Color(0xffE0E8EE);
+Color primaryColorShade7 = const Color(0xffECF2F6);
+Color primaryColorShade8 = const Color(0xffCDE3F5);
+
+
+// common colors
+Color white = Colors.white;
+Color greenColor = Colors.green;
+Color blackColor = Colors.black;
+Color orangeColor = Colors.orange;
+Color redColor = Colors.red;
+Color transparentColor = Colors.transparent;
